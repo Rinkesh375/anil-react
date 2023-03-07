89fbd3752112616451e34247d98cbbef380c4afa
rinkeshujjwal16@gmail.com <rinkeshujjwal16@gmail.com>
2023-03-07 14:12:31 +0530

    form and input data receving

--- video-15/src/App.js
+++ video-15/src/App.js
@@ -1,13 +1,42 @@
-import React,{useState} from 'react';
+import React from 'react';
 
 const App = () => {
- const [value,setValue] = useState(false)
+  const getFormData = (e)=>{
+        e.preventDefault()
+        console.log("input",e.target[0].value)
+        console.log("select",e.target[1].value)
+        console.log("input",e.target[2].checked)
+       
+  }
   return (
     <div className='App'>
-        {value && <h1>My name is Rinkesh</h1>}
-        <button onClick={()=>setValue(!value)}>{value?"Hide":"Show"}</button>
+      <form onSubmit={getFormData}>
+      <input type="text" />
+      <select name="course" id="course">
+            <option value="">Select option</option>
+            <option value="DSA">DSA</option>
+            <option value="Angular">Angular</option>
+            <option value="React">React</option>
+            <option value="Javascript">Javascript</option>
+
+            </select>
+            <input type="checkbox" name="condition" id="condition" /> <span>Accept condition</span>
+            <input type="submit" value="Submit" />
+        {/* <div><input type="text" /></div>
+        <div><select name="course" id="course">
+            <option value="">Select option</option>
+            <option value="DSA">DSA</option>
+            <option value="Angular">Angular</option>
+            <option value="React">React</option>
+            <option value="Javascript">Javascript</option>
+
+            </select></div>
+            <div> <input type="checkbox" name="condition" id="condition" /> <span>Accept condition</span></div> */}
+           
+      </form>
     </div>
   );
 }
 
 export default App;
+
