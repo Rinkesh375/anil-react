a0d9a760107ee70c61d931afdce20504a4fdad1f
rinkeshujjwal16@gmail.com <rinkeshujjwal16@gmail.com>
2023-03-07 19:31:02 +0530

    condition userLogin Rendering

--- video-15/src/App.js
+++ video-15/src/App.js
@@ -1,42 +1,13 @@
 import React from 'react';
+import Login from './Login';
 
 const App = () => {
-  const getFormData = (e)=>{
-        e.preventDefault()
-        console.log("input",e.target[0].value)
-        console.log("select",e.target[1].value)
-        console.log("input",e.target[2].checked)
-       
-  }
+  
   return (
     <div className='App'>
-      <form onSubmit={getFormData}>
-      <input type="text" />
-      <select name="course" id="course">
-            <option value="">Select option</option>
-            <option value="DSA">DSA</option>
-            <option value="Angular">Angular</option>
-            <option value="React">React</option>
-            <option value="Javascript">Javascript</option>
-
-            </select>
-            <input type="checkbox" name="condition" id="condition" /> <span>Accept condition</span>
-            <input type="submit" value="Submit" />
-        {/* <div><input type="text" /></div>
-        <div><select name="course" id="course">
-            <option value="">Select option</option>
-            <option value="DSA">DSA</option>
-            <option value="Angular">Angular</option>
-            <option value="React">React</option>
-            <option value="Javascript">Javascript</option>
-
-            </select></div>
-            <div> <input type="checkbox" name="condition" id="condition" /> <span>Accept condition</span></div> */}
-           
-      </form>
+      <Login/>
     </div>
   );
 }
 
 export default App;
-
