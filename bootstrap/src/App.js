bfe11ec8dc05bf8ec52700d609fcc15cbfed35ba
rinkeshujjwal16@gmail.com <rinkeshujjwal16@gmail.com>
2023-03-08 17:21:11 +0530

    using loop map for compoent and passing value directly object

--- bootstrap/src/App.js
+++ bootstrap/src/App.js
@@ -1,46 +1,19 @@
 // import logo from './logo.svg';
 import './App.css';
-import {Table} from 'react-bootstrap';
+import User from './User';
+// import {Table} from 'react-bootstrap';
 
 function App() {
   let userArr = [
-    {name:"Rinkesh",course:"FSD",address:[{house:698,city:"Faridabad",state:"Haryana"},{house:700,city:"Gurgoan",state:"Haryana"},{house:701,city:"Delhi",state:"Delhi"}]},
-    {name:"Nitish",course:"Backend",address:[{house:698,city:"Faridabad",state:"Haryana"},{house:700,city:"Gurgoan",state:"Haryana"},{house:701,city:"Delhi",state:"Delhi"}]},
-    {name:"Shekhar",course:"Machince Learing",address:[{house:698,city:"Faridabad",state:"Haryana"},{house:700,city:"Gurgoan",state:"Haryana"},{house:701,city:"Delhi",state:"Delhi"}]},
-    {name:"Akthar",course:"Java Developer",address:[{house:698,city:"Faridabad",state:"Haryana"},{house:700,city:"Gurgoan",state:"Haryana"},{house:701,city:"Delhi",state:"Delhi"}]},
-    {name:"Abishek",course:"Data Analtics",address:[{house:698,city:"Faridabad",state:"Haryana"},{house:700,city:"Gurgoan",state:"Haryana"},{house:701,city:"Delhi",state:"Delhi"}]}
+    {name:"Rinkesh",course:"FSD"},
+    {name:"Nitish",course:"Backend"},
+    {name:"Shekhar",course:"Machince Learing"},
+    {name:"Akthar",course:"Java Developer"},
+    {name:"Abishek",course:"Data Analtics"}
   ]
   return (
     <div className="App">
-      <Table variant="dark">
-        <thead><tr><th>S.NO</th><th>Name</th><th>Course</th><th>Address</th></tr></thead>
-        <tbody>
-          {userArr.map((person,index)=>{
-           return   <tr key={index}>
-               <td>{index+1}</td>
-              <td>{person.name}</td>
-              <td>{person.course}</td>
-               <td>
-                <Table variant="dark">
-                  <tbody>
-                {
-                person.address.map((houseAdd)=>{
-                  
-                  return <tr  >
-                    <td>House No- {houseAdd.house}</td>
-                    <td>City- {houseAdd.city}</td>
-                    <td>State- {houseAdd.state}</td>
-                  </tr>
-                })
-                }
-                </tbody>
-                </Table>
-                </td>
-                
-            </tr>
-          })}
-        </tbody>
-      </Table>
+     {userArr.map((ele,index)=> <User key={index} {...ele}/>)}
     </div>
   );
 }
---
+++ bootstrap/src/User.jsx
@@ -0,0 +1,13 @@
+
+
+const User = (ele) => {
+
+  return (
+    <div style={{display:"flex",flexDirection:"column",background:"#e5adad",margin:"1rem"}}>
+      <h1>Name : {ele.name}</h1>
+      <h1>Course : {ele.name}</h1>
+    </div>
+  );
+}
+
+export default User;
