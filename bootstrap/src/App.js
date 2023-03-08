d8cf21887dbd43513c4a58b53a30b58177f03a7f
rinkeshujjwal16@gmail.com <rinkeshujjwal16@gmail.com>
2023-03-08 18:18:55 +0530

    PureComponent for state and props only for class based component

--- bootstrap/src/App.js
+++ bootstrap/src/App.js
@@ -1,21 +1,43 @@
 // import logo from './logo.svg';
 import './App.css';
-import User from './User';
+// import  {PureComponent } from 'react';
+import  {Component } from 'react';
+// import User from './User';
 // import {Table} from 'react-bootstrap';
 
-function App() {
-  let userArr = [
-    {name:"Rinkesh",course:"FSD"},
-    {name:"Nitish",course:"Backend"},
-    {name:"Shekhar",course:"Machince Learing"},
-    {name:"Akthar",course:"Java Developer"},
-    {name:"Abishek",course:"Data Analtics"}
-  ]
-  return (
-    <div className="App">
-     {userArr.map((ele,index)=> <User key={index} {...ele}/>)}
-    </div>
-  );
-}
 
-export default App;
+ export default class App extends Component {
+  constructor(){
+    super()
+    this.state = {count:1}
+  }
+  render() {
+    console.log("Re-render")
+    return (
+      <div className='App'>
+        <h1>{this.state.count}</h1>
+        <button onClick={()=>this.setState({count:this.state.count+1})}>Click</button>
+      </div>
+    );
+  }
+} 
+
+
+
+// "---------------------------------------------------------------------------"
+/*  export default class App extends PureComponent {
+  constructor(){
+    super()
+    this.state = {count:1}
+  }
+  render() {
+    console.log("Re-render")
+    return (
+      <div className='App'>
+        <h1>{this.state.count}</h1>
+        <button onClick={()=>this.setState({count:this.state.count+1})}>Click</button>
+      </div>
+    );
+  }
+}   */
+
