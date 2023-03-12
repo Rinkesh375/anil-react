76ad26035af74f4bccca2ce42ad729e8481e6d1b
rinkeshujjwal16@gmail.com <rinkeshujjwal16@gmail.com>
2023-03-12 10:31:07 +0530

    Router video-2

--- useMemo/src/App.js
+++ useMemo/src/App.js
@@ -3,17 +3,21 @@ import './App.css';
 
 import React,{useState} from 'react';
 import Home from "./component/Home.jsx"
-import About from './component/About';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from './component/About.jsx';
+import { BrowserRouter, Route, Routes,Link } from "react-router-dom";
+import Navbar from './component/Navbar';
 
 
 const App = () => {
   return (
   <>
     <BrowserRouter>
+{/* <Link to="/" style={{margin:"1rem"}}>Home</Link> 
+    <Link to="/about">About</Link>  */}
+    <Navbar/>
     <Routes>
-      <Route exact path="/" element={<Home />} />
-      <Route exact path='/about' element={<About/>} />
+      <Route  path="/" element={<Home />} />
+      <Route  path='/about' element={<About/>} />
     </Routes>
     </BrowserRouter>
   </>
--- useMemo/src/component/Home.jsx
+++ useMemo/src/component/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
+    
     <div>
+        <Link to="/about">About</Link>
       <h1>I am Home</h1>
     </div>
   );
